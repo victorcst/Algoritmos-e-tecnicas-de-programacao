2f0cb3d5f05b9ad922ecea9b226f8c2b4ffde919
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-21 11:22:15 -0300

    Add files via upload

---
+++ Lista06/ex02.java
@@ -0,0 +1,24 @@
+public class ex02 {
+    public static void main(String[] args) {
+        int num;
+
+        for(num = 1; num <= 100; num++){
+            System.out.println(num);
+        }
+
+        num=1;
+
+        while(num<=100){
+            System.out.println(num);
+            num++;
+        }
+
+        num=0;
+
+        do{
+            System.out.println(num);
+            num++;
+        }while(num<=100);
+    }
+    
+}
