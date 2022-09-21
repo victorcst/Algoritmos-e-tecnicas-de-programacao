4e356e7619aadb55c9a6370ece5aacc7b42e24d4
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-21 11:26:54 -0300

    Add files via upload

---
+++ Lista06/ex10.java
@@ -0,0 +1,10 @@
+public class ex10 {
+    public static void main(String[] args) {
+        int num;
+        for(num=0; num<=100;num++) {
+            if(num%2==00){
+                System.out.println(num);
+            }
+        }
+    }
+}
