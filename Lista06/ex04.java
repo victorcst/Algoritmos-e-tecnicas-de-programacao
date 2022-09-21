fda7d06491afc2e4f80de45494a8019f407c590b
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-21 11:22:42 -0300

    Add files via upload

---
+++ Lista06/ex04.java
@@ -0,0 +1,10 @@
+public class ex04 {
+    public static void main(String[] args) {
+        int num;
+
+        for(num = 0 ; num <= 100000 ; num++){
+            System.out.println(num);
+            num = num+999;
+        }
+    }
+}
