db47e64aed89918501eca36583918d36d4851e72
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-21 11:22:28 -0300

    Add files via upload

---
+++ Lista06/ex03.java
@@ -0,0 +1,12 @@
+public class ex03 {
+    public static void main(String[] args) {
+        int num=10;
+        
+        while(num>0){
+            System.out.println(num);
+            num--;
+        }
+
+        System.out.println("Fim");
+    }
+}
