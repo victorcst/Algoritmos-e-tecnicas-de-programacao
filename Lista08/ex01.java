c50a4a19fc6236dcc8d94a2eaba02d6547ccec16
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-23 15:41:04 -0300

    Add files via upload

---
+++ Lista08/ex01.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int a, b;
+            int n;
+            int aux, resDiv, countRes = 0;
+            System.out.println("Insira 2 numeros: ");
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            if (a < b) {
+                if (a > 1) {
+                    for (n = a; n <= b; n++) {
+                        for (aux = 2; aux < (n - 1); aux++) {
+                            resDiv = n % aux;
+                            if (resDiv == 0) {
+                                countRes++;
+                            }
+                        }
+                        if (countRes == 0) {
+                            System.out.println(n);
+                        }
+                        countRes = 0;
+                    }
+                }
+            } else if (b < a) {
+                if (b > 1) {
+                    for (n = b; n <= a; n++) {
+                        for (aux = 2; aux < (n - 1); aux++) {
+                            resDiv = n % aux;
+                            if (resDiv == 0) {
+                                countRes++;
+                            }
+                        }
+                        if (countRes == 0) {
+                            System.out.println(n);
+                        }
+                        countRes = 0;
+                    }
+                }
+            }
+        }
+    }
+}
