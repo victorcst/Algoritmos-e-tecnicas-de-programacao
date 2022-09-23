0eb94dff54fb1e8b2ec577c143e9bbf20cf92629
victorcst <93409255+victorcst@users.noreply.github.com>
2022-09-23 15:43:19 -0300

    Add files via upload

---
+++ Lista08/ex06.java
@@ -0,0 +1,18 @@
+public class ex06 {
+    public static void main(String[] args) {
+        int num09, num1019, num200900, total=0;
+
+        num09=37;
+        num1019=63;
+        num200900=78;
+
+        num09=(num09*8)*9;
+        num1019=(num1019*1)*9;
+        num200900=(num200900*100);
+        
+
+        total = (num09+num1019)+num200900;
+
+        System.out.println(total);
+    }
+}
